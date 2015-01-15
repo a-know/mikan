00fa2be243f9ee3caef384db410ae5d1f2bea160
a-know <a.know.3373@gmail.com>
2015-01-15 14:36:42 +0900

    Change `mikanzs` null constraint and index

--- db/migrate/20150115053044_create_mikanzs.rb
+++ db/migrate/20150115053044_create_mikanzs.rb
@@ -2,11 +2,13 @@ class CreateMikanzs < ActiveRecord::Migration
   def change
     create_table :mikanzs do |t|
       t.integer :owner_id
-      t.string :name
-      t.datetime :start_time
-      t.text :content
+      t.string :name,         null: false
+      t.datetime :start_time, null: false
+      t.text :content,        null: false
 
-      t.timestamps null: false
+      t.timestamps
     end
+
+    add_index :mikanzs, :owner_id
   end
 end
