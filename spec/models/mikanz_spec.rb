bac18cd6b8d9a64e77a486309c0c09d15c593d4b
a-know <a.know.3373@gmail.com>
2015-01-15 15:32:53 +0900

    Add validation spec

--- spec/models/mikanz_spec.rb
+++ spec/models/mikanz_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Mikanz, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(50) }
+  end
+  describe '#content' do
+    it { should validate_presence_of(:content) }
+    it { should ensure_length_of(:content).is_at_most(2000) }
+  end
+  describe '#start_time' do
+    it { should validate_presence_of(:start_time) }
+  end
 end
