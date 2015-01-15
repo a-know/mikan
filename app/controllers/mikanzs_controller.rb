5fa73ca9387646a97d2d22759e0dc888fdf863c0
a-know <a.know.3373@gmail.com>
2015-01-15 15:20:35 +0900

    Implement for register mikanz

--- app/controllers/mikanzs_controller.rb
+++ app/controllers/mikanzs_controller.rb
@@ -1,2 +1,24 @@
+# encoding: utf-8
+
 class MikanzsController < ApplicationController
+  before_action :authenticate
+
+  def new
+    @mikanz = current_user.created_mikanzs.build
+  end
+
+  def create
+    @mikanz = current_user.created_mikanzs.build(mikanz_param)
+    if @mikanz.save
+      redirect_to @mikanz, notice: '登録しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def mikanz_param
+    params.require(:mikanz).permit(:name, :content, :start_time)
+  end
 end
--- app/models/mikanz.rb
+++ app/models/mikanz.rb
@@ -2,5 +2,4 @@ class Mikanz < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
   validates :content, length: { maximum: 2000 }, presence: true
   validates :start_time, presence: true
-  validate  :start_time_should_be_before_end_time
 end
