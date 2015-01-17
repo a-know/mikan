31eaa29dfae2ec6294d6bc4bc11054d1c2aa7276
a-know <a.know.3373@gmail.com>
2015-01-17 11:52:30 +0900

    mikanz#create のテストを補完

--- spec/controllers/mikanzs_controller_spec.rb
+++ spec/controllers/mikanzs_controller_spec.rb
@@ -32,6 +32,48 @@ RSpec.describe MikanzsController, :type => :controller do
     end
   end
 
+  describe 'POST#create' do
+    before { @params = attributes_for(:mikanz) }
+    subject { post :create, mikanz: @params }
+
+    context 'when user not logged in accessed' do
+      it 'redirect to top' do
+        subject
+        expect(response).to redirect_to(root_path)
+      end
+    end
+    context 'params are all correctly and save successful' do
+      before do
+        user = create(:user)
+        session[:user_id] = user.id
+      end
+
+      it 'return 200 as status code' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'redirect to  `show` page' do
+        subject
+        saved_mikanz = Mikanz.find_by(name: @params[:name])
+        expect(response).to redirect_to(mikanz_url(id: saved_mikanz.id))
+      end
+
+      it 'set a saved object to @mikanz' do
+        subject
+        expect(assigns(:mikanz).errors).to be_empty
+        expect(assigns(:mikanz)).to be_persisted
+        expect(assigns(:mikanz).name).to eq(@params[:name])
+      end
+
+      it 'create mikanz in DB successfuly' do
+        expect do
+          subject
+          response
+        end.to change{ Mikanz.count }.by(1)
+      end
+    end
+  end
+
   describe 'GET #show' do
     context 'when specified id which does not saved' do
       before { get :show, id: 99999 }
@@ -46,7 +88,8 @@ RSpec.describe MikanzsController, :type => :controller do
         get :show, id: @mikanz.id
       end
 
-      it 'return 200 as status code' do
+      it 'return 200 OK' do
+        expect(response).to be_success
         expect(response.status).to eq(200)
       end
 
