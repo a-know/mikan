575ff3a7f4739638a13b2cb7fff4ad01400c54ce
a-know <a.know.3373@gmail.com>
2015-01-18 09:30:35 +0900

    Fix factory data

--- spec/factories/mikanzs.rb
+++ spec/factories/mikanzs.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :mikanz do
-    owner_id 1
-    name "MyString"
+    sequence(:owner_id) { |i| i }
+    sequence(:name) { |i| "MyString-#{i}" }
     start_time "2015-01-15 14:30:44"
     content "MyText"
   end
-
 end
