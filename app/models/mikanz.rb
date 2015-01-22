d4e9446033e27537cc279d889c73e6ef7be899f3
a-know <a.know.3373@gmail.com>
2015-01-22 20:52:44 +0900

    Add definition for paranoia

--- app/models/mikanz.rb
+++ app/models/mikanz.rb
@@ -1,4 +1,5 @@
 class Mikanz < ActiveRecord::Base
+  acts_as_paranoid
   belongs_to :owner, class_name: 'User'
   validates :name, length: { maximum: 50 }, presence: true
   validates :content, length: { maximum: 2000 }, presence: true
