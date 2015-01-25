61f5bea856e9e3b401eca271c776c2c053104618
a-know <a.know.3373@gmail.com>
2015-01-25 10:38:36 +0900

    Change data type of completion column

--- db/migrate/20150125013449_change_completion_of_mikanzs.rb
+++ db/migrate/20150125013449_change_completion_of_mikanzs.rb
@@ -1,4 +1,9 @@
 class ChangeCompletionOfMikanzs < ActiveRecord::Migration
-  def change
+  def self.up
+    change_column :mikanzs, :completion, :integer, :null => false
+  end
+
+  def self.down
+    change_column :mikanzs, :completion, :string,  :null => true
   end
 end
