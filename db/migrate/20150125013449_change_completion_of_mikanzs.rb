a91c241e64c108d4ff71f5f6a5a3a60529b3efd7
a-know <a.know.3373@gmail.com>
2015-01-25 10:44:56 +0900

    Add cast to integer from string

--- db/migrate/20150125013449_change_completion_of_mikanzs.rb
+++ db/migrate/20150125013449_change_completion_of_mikanzs.rb
@@ -1,9 +1,9 @@
 class ChangeCompletionOfMikanzs < ActiveRecord::Migration
   def self.up
-    change_column :mikanzs, :completion, :integer, :null => false
+    change_column :mikanzs, :completion, 'integer USING CAST(completion AS integer)'
   end
 
   def self.down
-    change_column :mikanzs, :completion, :string,  :null => true
+    change_column :mikanzs, :completion, 'string USING CAST(completion AS string)'
   end
 end
