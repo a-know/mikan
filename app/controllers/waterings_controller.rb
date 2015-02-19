7477ab5806402976816f4344e77e9783ba14fb2a
a-know <a.know.3373@gmail.com>
2015-02-19 21:02:44 +0900

    Implement `#new`

--- app/controllers/waterings_controller.rb
+++ app/controllers/waterings_controller.rb
@@ -1,2 +1,9 @@
+# encoding: utf-8
+
 class WateringsController < ApplicationController
+  before_action :authenticate
+
+  def new
+    raise ActionController::RoutingError, 'ログイン状態で WateringsController#new にアクセスされました'
+  end
 end
--- spec/controllers/waterings_controller_spec.rb
+++ spec/controllers/waterings_controller_spec.rb
@@ -1,5 +1,21 @@
+# encoding: utf-8
+
 require 'rails_helper'
 
 RSpec.describe WateringsController, type: :controller do
-
+  describe '#new' do
+    subject { get :new, :mikanz_id => @mikanz.id }
+    context 'ログイン状態で WateringsController#new にアクセス' do
+      before do
+        @mikanz = create(:mikanz)
+        user = create(:user)
+        session[:user_id] = user.id
+      end
+      it 'ActionController::RoutingError がスローされること' do
+        expect do
+          subject
+        end.to raise_error(ActionController::RoutingError)
+      end
+    end
+  end
 end
