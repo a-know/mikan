a6525b7a4100bd4508c555654640880782a68d3d
a-know <a.know.3373@gmail.com>
2015-03-01 11:43:53 +0900

    `poltergesit` 利用設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,16 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'rspec/autorun'
+
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
 
 require 'factory_girl_rails'
+
+Capybara.javascript_driver = :poltergeist
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
