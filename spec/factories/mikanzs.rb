945207bb8653fceefdfc667f865d243cbe2530b9
a-know <a.know.3373@gmail.com>
2015-05-07 20:01:31 +0900

    項目 `completion` を、完成度を表す enum に変更

--- spec/factories/mikanzs.rb
+++ spec/factories/mikanzs.rb
@@ -22,7 +22,7 @@ FactoryGirl.define do
     sequence(:name) { |i| "MyString-#{i}" }
     start_time "2015-01-15 14:30:44"
     content "MyText"
-    completion 55
+    completion 'wip'
     sequence(:mikanz_image)       { |i| "MyString-#{i}.jpg" }
     sequence(:mikanz_image_cache) { |i| "MyString-#{i}.jpg.cache" }
     remove_mikanz_image false
