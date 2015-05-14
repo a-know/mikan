5fecd2b37bf1ac648be4af514e29217746aa30c9
a-know <a.know.3373@gmail.com>
2015-05-14 21:26:50 +0900

    watering に関する不要な route を除外

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,31 @@
 # == Route Map
 #
-#               Prefix Verb   URI Pattern                                      Controller#Action
-#              mikanzs GET    /mikanzs(.:format)                               mikanzs#index
-#                      POST   /mikanzs(.:format)                               mikanzs#create
-#           new_mikanz GET    /mikanzs/new(.:format)                           mikanzs#new
-#          edit_mikanz GET    /mikanzs/:id/edit(.:format)                      mikanzs#edit
-#               mikanz GET    /mikanzs/:id(.:format)                           mikanzs#show
-#                      PATCH  /mikanzs/:id(.:format)                           mikanzs#update
-#                      PUT    /mikanzs/:id(.:format)                           mikanzs#update
-#                      DELETE /mikanzs/:id(.:format)                           mikanzs#destroy
-#                 root GET    /                                                top#index
-#                      GET    /auth/:provider/callback(.:format)               sessions#create
-#               logout GET    /logout(.:format)                                sessions#destroy
-#          retire_user GET    /user/retire(.:format)                           users#retire
-#                 user DELETE /user(.:format)                                  users#destroy
-#   tag_search_mikanzs GET    /mikanzs/tag_search(.:format)                    mikanzs#tag_search
-#     mikanz_waterings GET    /mikanzs/:mikanz_id/waterings(.:format)          waterings#index
-#                      POST   /mikanzs/:mikanz_id/waterings(.:format)          waterings#create
-#  new_mikanz_watering GET    /mikanzs/:mikanz_id/waterings/new(.:format)      waterings#new
-# edit_mikanz_watering GET    /mikanzs/:mikanz_id/waterings/:id/edit(.:format) waterings#edit
-#      mikanz_watering GET    /mikanzs/:mikanz_id/waterings/:id(.:format)      waterings#show
-#                      PATCH  /mikanzs/:mikanz_id/waterings/:id(.:format)      waterings#update
-#                      PUT    /mikanzs/:mikanz_id/waterings/:id(.:format)      waterings#update
-#                      DELETE /mikanzs/:mikanz_id/waterings/:id(.:format)      waterings#destroy
-#                      GET    /mikanzs(.:format)                               mikanzs#index
-#                      POST   /mikanzs(.:format)                               mikanzs#create
-#                      GET    /mikanzs/new(.:format)                           mikanzs#new
-#                      GET    /mikanzs/:id/edit(.:format)                      mikanzs#edit
-#                      GET    /mikanzs/:id(.:format)                           mikanzs#show
-#                      PATCH  /mikanzs/:id(.:format)                           mikanzs#update
-#                      PUT    /mikanzs/:id(.:format)                           mikanzs#update
-#                      DELETE /mikanzs/:id(.:format)                           mikanzs#destroy
+#              Prefix Verb   URI Pattern                                 Controller#Action
+#             mikanzs GET    /mikanzs(.:format)                          mikanzs#index
+#                     POST   /mikanzs(.:format)                          mikanzs#create
+#          new_mikanz GET    /mikanzs/new(.:format)                      mikanzs#new
+#         edit_mikanz GET    /mikanzs/:id/edit(.:format)                 mikanzs#edit
+#              mikanz GET    /mikanzs/:id(.:format)                      mikanzs#show
+#                     PATCH  /mikanzs/:id(.:format)                      mikanzs#update
+#                     PUT    /mikanzs/:id(.:format)                      mikanzs#update
+#                     DELETE /mikanzs/:id(.:format)                      mikanzs#destroy
+#                root GET    /                                           top#index
+#                     GET    /auth/:provider/callback(.:format)          sessions#create
+#              logout GET    /logout(.:format)                           sessions#destroy
+#         retire_user GET    /user/retire(.:format)                      users#retire
+#                user DELETE /user(.:format)                             users#destroy
+#  tag_search_mikanzs GET    /mikanzs/tag_search(.:format)               mikanzs#tag_search
+#    mikanz_waterings POST   /mikanzs/:mikanz_id/waterings(.:format)     waterings#create
+# new_mikanz_watering GET    /mikanzs/:mikanz_id/waterings/new(.:format) waterings#new
+#     mikanz_watering DELETE /mikanzs/:mikanz_id/waterings/:id(.:format) waterings#destroy
+#                     GET    /mikanzs(.:format)                          mikanzs#index
+#                     POST   /mikanzs(.:format)                          mikanzs#create
+#                     GET    /mikanzs/new(.:format)                      mikanzs#new
+#                     GET    /mikanzs/:id/edit(.:format)                 mikanzs#edit
+#                     GET    /mikanzs/:id(.:format)                      mikanzs#show
+#                     PATCH  /mikanzs/:id(.:format)                      mikanzs#update
+#                     PUT    /mikanzs/:id(.:format)                      mikanzs#update
+#                     DELETE /mikanzs/:id(.:format)                      mikanzs#destroy
 #
 
 Rails.application.routes.draw do
@@ -48,6 +43,6 @@ Rails.application.routes.draw do
     collection do
       get 'tag_search'
     end
-    resources :waterings
+    resources :waterings, only: [:new, :create, :destroy]
   end
 end
