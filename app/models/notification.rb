134bdccbb861826efd753b383bb9cd7ec692f5e9
a-know <a.know.3373@gmail.com>
2015-05-28 20:41:14 +0900

    bundle exec annotate

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  watering_id :integer
+#  kind        :integer          not null
+#  read        :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Notification < ActiveRecord::Base
   belongs_to :user
   belongs_to :watering
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  watering_id :integer
+#  kind        :integer          not null
+#  read        :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
