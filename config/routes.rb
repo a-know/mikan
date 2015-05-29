433e0ea9f677ba30a481087f41f63427ef43100f
a-know <a.know.3373@gmail.com>
2015-05-29 19:29:51 +0900

    通知一覧への route を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 # == Route Map
 #
-#              Prefix Verb   URI Pattern                                 Controller#Action
-#                root GET    /                                           top#index
-#                     GET    /auth/:provider/callback(.:format)          sessions#create
-#              logout GET    /logout(.:format)                           sessions#destroy
-#         retire_user GET    /user/retire(.:format)                      users#retire
-#                user DELETE /user(.:format)                             users#destroy
-#        user_mikanzs GET    /users/:user_nickname/mikanzs(.:format)     mikanzs#users_mikanzs
-#  tag_search_mikanzs GET    /mikanzs/tag_search(.:format)               mikanzs#tag_search
-#    mikanz_waterings POST   /mikanzs/:mikanz_id/waterings(.:format)     waterings#create
-# new_mikanz_watering GET    /mikanzs/:mikanz_id/waterings/new(.:format) waterings#new
-#     mikanz_watering DELETE /mikanzs/:mikanz_id/waterings/:id(.:format) waterings#destroy
-#             mikanzs GET    /mikanzs(.:format)                          mikanzs#index
-#                     POST   /mikanzs(.:format)                          mikanzs#create
-#          new_mikanz GET    /mikanzs/new(.:format)                      mikanzs#new
-#         edit_mikanz GET    /mikanzs/:id/edit(.:format)                 mikanzs#edit
-#              mikanz GET    /mikanzs/:id(.:format)                      mikanzs#show
-#                     PATCH  /mikanzs/:id(.:format)                      mikanzs#update
-#                     PUT    /mikanzs/:id(.:format)                      mikanzs#update
-#                     DELETE /mikanzs/:id(.:format)                      mikanzs#destroy
+#              Prefix Verb   URI Pattern                                   Controller#Action
+#                root GET    /                                             top#index
+#                     GET    /auth/:provider/callback(.:format)            sessions#create
+#              logout GET    /logout(.:format)                             sessions#destroy
+#         retire_user GET    /user/retire(.:format)                        users#retire
+#                user DELETE /user(.:format)                               users#destroy
+#        user_mikanzs GET    /users/:user_nickname/mikanzs(.:format)       mikanzs#users_mikanzs
+#  user_notifications GET    /users/:user_nickname/notifications(.:format) users#notifications
+#  tag_search_mikanzs GET    /mikanzs/tag_search(.:format)                 mikanzs#tag_search
+#    mikanz_waterings POST   /mikanzs/:mikanz_id/waterings(.:format)       waterings#create
+# new_mikanz_watering GET    /mikanzs/:mikanz_id/waterings/new(.:format)   waterings#new
+#     mikanz_watering DELETE /mikanzs/:mikanz_id/waterings/:id(.:format)   waterings#destroy
+#             mikanzs GET    /mikanzs(.:format)                            mikanzs#index
+#                     POST   /mikanzs(.:format)                            mikanzs#create
+#          new_mikanz GET    /mikanzs/new(.:format)                        mikanzs#new
+#         edit_mikanz GET    /mikanzs/:id/edit(.:format)                   mikanzs#edit
+#              mikanz GET    /mikanzs/:id(.:format)                        mikanzs#show
+#                     PATCH  /mikanzs/:id(.:format)                        mikanzs#update
+#                     PUT    /mikanzs/:id(.:format)                        mikanzs#update
+#                     DELETE /mikanzs/:id(.:format)                        mikanzs#destroy
 #
 
 Rails.application.routes.draw do
@@ -34,6 +35,7 @@ Rails.application.routes.draw do
     scope controller: :mikanzs do
       get :mikanzs, action: :users_mikanzs
     end
+    get 'notifications'
   end
 
   resources :mikanzs do
