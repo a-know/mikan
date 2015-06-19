cac15bc401d327393b16558bbc43bf6d43e0ba86
a-know <a.know.3373@gmail.com>
2015-06-19 13:52:55 +0900

    最近登録されたミカンは4つ返すようにした

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @mikanzs = Mikanz.order(:created_at).reverse_order.limit(3)
+    @mikanzs = Mikanz.order(:created_at).reverse_order.limit(4)
     @tags = Mikanz.tag_counts_on(:tags).order('count DESC, name ASC').sort
   end
 end
