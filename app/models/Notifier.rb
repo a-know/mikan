d84b004b64701f21e627c13a1851209bfb4f7dac
a-know <a.know.3373@gmail.com>
2015-06-23 00:28:59 +0900

    oops

--- app/models/Notifier.rb
+++
@@ -1,9 +0,0 @@
-class Notifier
-  def initialize
-    @slack_notifier ||= Slack::Notifier.new ENV['SLACK_WEBHOOK_URL'], channel: '#mikanz-notification'
-  end
-
-  def post(str)
-    @slack_notifier.ping([ '[', Rails.env, '] ' ].join + str) unless Rails.env == 'test'
-  end
-end
