eca781308bb2298540a49dea9df2e61bd6d04c6a
a-know <a.know.3373@gmail.com>
2015-06-24 00:54:25 +0900

    PR デプロイ先を変更

--- scripts/feature_deploy.sh
+++ scripts/feature_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-HEROKU_APPS="mikans-stg"
+HEROKU_APPS="mikanz-stg"
 
 DEPLOY_SCRIPT=/tmp/deploy.$$.sh
 curl https://s3-ap-northeast-1.amazonaws.com/shitemil.com/scripts/heroku_production_deploy.sh.txt > ${DEPLOY_SCRIPT}
