faadb26e9c88e5b50998dbe60228343ace91e351
a-know <a.know.3373@gmail.com>
2015-08-31 16:37:14 +0900

    Add configuration for capistrano3/unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,11 @@ set :scm, :git
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, (fetch(:linked_dirs) + ['tmp/pids'])
+
+# Unicorn
+set :unicorn_rack_env, 'none'
+set :unicorn_config_path, 'config/unicorn.rb'
 
 # Default value for default_env is {}
 set :default_env, {
@@ -39,6 +43,7 @@ set :rails_env, :iaas
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
 
   after :restart, :clear_cache do
@@ -48,6 +53,10 @@ namespace :deploy do
       #   execute :rake, 'cache:clear'
       # end
     end
+  end
+
+  task :restart do
+    invoke 'unicorn:restart'
   end
 
 end
