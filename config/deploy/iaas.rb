f5a7228948660084ea9b83408607f490d236bdbd
a-know <a.know.3373@gmail.com>
2015-09-06 18:48:58 +0900

    Resolve postgresql path

--- config/deploy/iaas.rb
+++ config/deploy/iaas.rb
@@ -1,3 +1,6 @@
+default_env = { path: "/usr/pgsql-9.4/bin:$PATH" }
+set :default_env, default_env
+
 role :app, %w{a-know@mikanz-test.gce}
 role :web, %w{a-know@mikanz-test.gce}
 role :db , %w{a-know@mikanz-test.gce}
